5732f1a08acd0ee78df5875f32557e31a0a4f0d1
Azrubael <i0638464000@gmail.com>
2026-06-10 17:00:48 +0300

    Updated the directory '260607-gio-win/'.

--- 260607-gio-win/260610_run_window.go
+++ 260607-gio-win/260610_run_window.go
@@ -22,8 +22,6 @@ func run_window(window *app.Window) error {
 
 	theme := material.NewTheme()
 
-	window.Option(app.Title("GIO UI app"))
-	window.Option(app.Size(unit.Dp(320), unit.Dp(480)))
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
@@ -31,18 +29,15 @@ func run_window(window *app.Window) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 
-			// Menu bar
-			// Spacing: Leftover space will be at the start
 			layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceStart, Alignment: layout.Start}.Layout(gtx,
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 					d := material.Button(theme, fileBtn, "File").Layout(gtx)
-					if fileBtn.Clicked(gtx) {
+					if actionBtn.Clicked(gtx) {
 						fileOpen = !fileOpen
-						// close others if desired:
 						actionOpen = false
 						helpOpen = false
 					}
-					return d
+					return d					
 				}),
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 					d := material.Button(theme, actionBtn, "Action").Layout(gtx)
@@ -62,54 +57,22 @@ func run_window(window *app.Window) error {
 					}
 					return d
 				}),
-				layout.Rigid(
-					// The height of the spacer is 25 Device independent pixels
-					layout.Spacer{Height: unit.Dp(25)}.Layout,
-				),
 			)
-
-			// Simple dropdowns under the menu bar
-			// You can improve positioning with more advanced layout (e.g., layout.Stack).
+			
 			if fileOpen {
-				layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+				layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
 					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 						return material.Button(theme, new(widget.Clickable), "New").Layout(gtx)
 					}),
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return material.Button(theme, new(widget.Clickable), "Open").Layout(gtx)
-					}),
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return material.Button(theme, new(widget.Clickable), "Exit").Layout(gtx)
-					}),
-				)
-			}
-			if actionOpen {
-				layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return material.Button(theme, new(widget.Clickable), "Run").Layout(gtx)
-					}),
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return material.Button(theme, new(widget.Clickable), "Stop").Layout(gtx)
-					}),
-				)
-			}
-			if helpOpen {
-				layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return material.Button(theme, new(widget.Clickable), "About").Layout(gtx)
-					}),
 				)
 			}
 
-			// Center-alligned text in the main window
-			title := material.H6(theme, "Hello, Gio!")
-			title.Alignment = text.Middle
-
-			// Add some top padding so title isn't drawn on top of menu bar
+			title := material.H5(theme, "Hello, Gio")
+			title.Alignment = text.Start
 			layout.Inset{Top: unit.Dp(12)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 				return title.Layout(gtx)
 			})
-
+			
 			e.Frame(gtx.Ops)
 		}
 	}
