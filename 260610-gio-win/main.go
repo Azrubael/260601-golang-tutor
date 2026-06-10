76d0430b2d65d9e46130a1689fcb0f9010cf4361
Azrubael <i0638464000@gmail.com>
2026-06-10 17:02:59 +0300

    Added and tested '260610gio-win/'.

---
+++ 260610-gio-win/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"gioui.org/app"
+)
+
+func main() {
+	go func() {
+		window := new(app.Window)
+
+		err := run_window(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
