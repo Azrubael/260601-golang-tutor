bdb49d188f9158006c643ac7cb1b44804979cf48
Azrubael <i0638464000@gmail.com>
2026-06-11 08:51:56 +0300

    Updated 'sanbox/'.

---
+++ sandbox/loop_window.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func run_window(window *app.Window) error {
+	var ops op.Ops
+	var (
+		fileBtn   = new(widget.Clickable)
+		actionBtn = new(widget.Clickable)
+		helpBtn   = new(widget.Clickable)
+		fileOpen, actionOpen, helpOpen bool
+	)
+
+	theme := material.NewTheme()
+
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			layout.Flex{Axis: layout.Horizontal, Alignment: layout.Start}.Layout(gtx,
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					d := material.Button(theme, fileBtn, "File").Layout(gtx)
+					if fileBtn.Clicked(gtx) {
+						fileOpen = !fileOpen
+						actionOpen = false
+						helpOpen = false
+					}
+					return d
+				}),
+			)
+
+			if fileOpen {
+				layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						return material.Button(theme, new(widget.Clickable), "New").Layout(gtx)
+					}),
+				)
+			}
+
+			title := material.H5(theme, "Hello, Gio")
+			title.Alignment = text.Start
+			layout.Inset{Top: unit.Dp(12)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				return title.Layout(gtx)
+			})
+
+			e.Frame(gtx.Ops)
+		}
+	}
+}
