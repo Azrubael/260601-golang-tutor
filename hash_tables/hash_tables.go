a8333dc48ff5a6a02150b6b4ad3d23f999371230
Azrubael <i0638464000@gmail.com>
2026-06-16 18:30:23 +0300

    Updated 'hash_tables/hash_tables.go'.

--- hash_tables/hash_tables.go
+++ hash_tables/hash_tables.go
@@ -21,11 +21,30 @@ type bucketNode struct {
 	next *bucketNode
 }
 
-// insert
+// Insert - will take a key and add it to the hash table array
+func (h *HashTable) Insert(key string) {
+	index := hash(key)
+	h.array[index].insert(key)
+}
 
-// search
+// Search - will take a key and return true if the key is stored in the hash table
+func (h *HashTable) Search(key string) bool {
+		index := hash(key)
+}
 
-// delete
+// Delete - will take a key and telete the corresponding element from the hash table
+func (h *HashTable) Delete(key string) {
+		index := hash(key)
+}
+
+// hash - will take a key and return the result of hash function
+func hash(key string) int {
+	sum := 0
+	for _, v := range key{
+		sum += int(v)
+	}
+	return sum % ArraySize
+}
 
 // Init - will create a bucket in each slot of the hash table
 func Init() *HashTable {
@@ -36,7 +55,34 @@ func Init() *HashTable {
 	return result
 }
 
+// insert - will take a key, kreate a node with the key and insert the node in the bucket
+func (b *bucket) insert(k string) {
+	newNode := &bucketNode{key: k}
+	newNode.next = b.head
+	b.head = newNode
+}
+
+// search - will take a key and return true if the key is stored in the bucket
+func (b *bucket) search(k string) bool {
+	currentNode := b.head
+	for currentNode != nil {
+		if currentNode.key == k {
+			return true
+		}
+		currentNode = currentNode.next
+	}
+	return false
+}
+
+// delete -
+
 func main() {
 	testHashTable := Init()
 	fmt.Println(testHashTable)
+	fmt.Println(hash("RANDY")
+	
+	testBucket := &bucket{}
+	testBucket.insert("RANDY")
+	fmt.Println(testBucket.search("RANDY"))
+	fmt.Println(testBucket.search("RAIN"))
 }
